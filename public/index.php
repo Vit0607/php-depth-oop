38297ada13d2857f7d32085adca61bff80c8211d
Vitaliy Yurkin <68192985+Vit0607@users.noreply.github.com>
2026-02-02 21:18:44 +0300

    composer

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use Aura\SqlQuery\QueryFactory;
+
+$queryFactory = new QueryFactory('mysql');
+
+$select = $queryFactory->newSelect();
+
+$select->cols(['*'])->from('posts');
+
+// a PDO connection
+$pdo = new PDO("mysql:host=localhost;dbname=app3;charset=utf8", "root", "");
+
+// prepare the statement
+$sth = $pdo->prepare($select->getStatement());
+
+// bind the values and execute
+$sth->execute($select->getBindValues());
+
+// get the results back as an associative array
+$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($result);
+
+echo 123;
