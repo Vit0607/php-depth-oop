7a2a01c12e2199ee70724430d4b97f9aeaba4cf1
Vitaliy Yurkin <68192985+Vit0607@users.noreply.github.com>
2026-02-05 22:05:12 +0300

    QueryBuilder

--- app/QueryBuilder.php
+++ app/QueryBuilder.php
@@ -2,28 +2,91 @@
 
 namespace App;
 
+use Aura\SqlQuery\QueryFactory;
+
+use PDO;
+
 class QueryBuilder {
+    private $pdo;
+    private $queryFactory;
+
+    public function __construct() {
+        $this->pdo = new PDO("mysql:host=localhost;dbname=app3;charset=utf8", "root", "");
+        $this->queryFactory = new QueryFactory('mysql');
+    }
+
     public function getAll($table) {
 
-    // use Aura\SqlQuery\QueryFactory;
+        $select = $this->queryFactory->newSelect();
 
-    // $queryFactory = new QueryFactory('mysql');
+        $select->cols(['*'])->from($table);
 
-    // $select = $queryFactory->newSelect();
+        $sth = $this->pdo->prepare($select->getStatement());
 
-    // $select->cols(['*'])->from('posts');
+        $sth->execute($select->getBindValues());
 
-    // // a PDO connection
-    // $pdo = new PDO("mysql:host=localhost;dbname=app3;charset=utf8", "root", "");
+        $result = $sth->fetchAll(PDO::FETCH_ASSOC);
 
-    // // prepare the statement
-    // $sth = $pdo->prepare($select->getStatement());
+        return $result;
 
-    // // bind the values and execute
-    // $sth->execute($select->getBindValues());
+    }
 
-    // // get the results back as an associative array
-    // $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+    public function insert($data, $table) {
+        $insert = $this->queryFactory->newInsert();
 
+        $insert
+            ->into($table)
+            ->cols($data);
+            
+        $sth = $this->pdo->prepare($insert->getStatement());
+
+        $sth->execute($insert->getBindValues());
+
+        $insert->getLastInsertIdName('id');
+    }
+
+    public function update($table, $data, $id) {
+        $update = $this->queryFactory->newUpdate();
+
+        $update
+            ->table($table)                  
+            ->cols($data)
+            ->where('id = :id')
+            ->bindValue('id', $id);
+
+        $sth = $this->pdo->prepare($update->getStatement());
+
+        $sth->execute($update->getBindValues());
+    }
+
+    public function findOne($table, $id) {
+        $select = $this->queryFactory->newSelect();
+
+        $select
+            ->cols(['*'])
+            ->from($table)
+            ->where('id = :id')
+            ->bindValue('id', $id);;
+
+        $sth = $this->pdo->prepare($select->getStatement());
+
+        $sth->execute($select->getBindValues());
+
+        $result = $sth->fetch(PDO::FETCH_ASSOC);
+
+        return $result;
+    }
+
+    public function delete($table, $id) {
+        $delete = $this->queryFactory->newDelete();
+
+        $delete
+            ->from($table)
+            ->where('id = :id')
+            ->bindValue('id', $id);
+
+        $sth = $this->pdo->prepare($delete->getStatement());
+
+        $sth->execute($delete->getBindValues());
     }
 }
--- public/index.php
+++ public/index.php
@@ -2,8 +2,6 @@
 
 require '../vendor/autoload.php';
 
-use App\QueryBuilder;
-
-$db = new QueryBuilder;
-
-var_dump($db);
+if ($_SERVER['REQUEST_URI'] == '/home') {
+    require '../app/controllers/homepage.php';
+}
